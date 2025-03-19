14263ac6c7b58e97b07ed5e5ae5fc7bb458b0606
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-03-19 00:14:58 -0300

    feat: pequena alteração na tela inicial

--- views/startScreen/startScreen.c
+++ views/startScreen/startScreen.c
@@ -5,7 +5,11 @@
 #include "../../utils/printColorful/printColorful.h"
 
 void startScreen(){
-    system("figlet LeaseInsight");
+    printColorful("======================================\n", 3);
+    printColorful("===========  ", 3);
+    printColorful("LEASEINSIGHT", 3);
+    printColorful("  ===========\n", 3);
+    printColorful("======================================\n", 3);
     printColorful("Administração de locação de imóveis\n", 5);
     printColorful("Copyright © 2025 LeaseInsight. Todos os direitos reservados.\n", 5);
     printColorful("Desenvolvido por: Raphael e Ygor\n\n", 5);
